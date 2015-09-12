f768f7712598721c2d9666fd7f99a644300e448c
split750 <godefroy.lemerdy@gmail.com>
2015-09-12 14:25:56 +0200

    correct bug

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'keyboard cat'
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
